f4305bb52abe308b9ab62802c25b0ceb1f44a165
Zordan <gabriel_zordan@hotmail.com>
2023-03-20 18:14:15 -0300

    [CORE] List Screen - Database connection

--- src/components/list-item/view.js
+++ src/components/list-item/view.js
@@ -4,17 +4,18 @@ import { Input } from "../../components/ui-input/view";
 import './style.sass';
 
 function ListItem(props) {
-    const [edit, setEdit] = useState(props.edit);
-    const [name, setName] = useState(props.name);
-    const oldName = props.name;
+    const [edit, setEdit] = useState(props.item.edit);
+    const [name, setName] = useState(props.item.name);
+    const [done, setDone] = useState(props.item.done);
+    const [id] = useState(props.item.id);
     
   return (
     <div className="list-item">
         {edit === true ?
         <form className="edit-item" onSubmit={(e) => {
             e.preventDefault();
-            if(props.function && name.length > 0) {
-                props.function({"name": name, "oldName": oldName});
+            if(props.submit && name.length > 0) {
+                props.submit({"name": name, "id": id});
                 setEdit(false)
             }
         }}>
@@ -29,16 +30,32 @@ function ListItem(props) {
             />
         </form>
         : <>
-            <div className="item-text">
+            <div className={done === true ? "item-text done" : "item-text"} onClick={() => {
+                if(props.click) {
+                    props.click({"id": id, "done": !done});
+                    setDone(!done);
+                }
+            }}>
                 {name}
             </div>
-            <IconButton
-                name="pencil"
-                color="button-light"
-                size="sm"
-                function={() => {
-                setEdit(true);
-            }} />
+            {done === true ? 
+                <IconButton
+                    name={"close"}
+                    color="button-light"
+                    size="sm"
+                    function={() => {
+                    if(props.delete){
+                        props.delete({"id": id});
+                    }
+                }} />
+                : <IconButton
+                    name={"pencil"}
+                    color="button-light"
+                    size="sm"
+                    function={() => {
+                    setEdit(true);
+                }} />
+            }
         </>
         }
     </div>
--- src/pages/list/view.js
+++ src/pages/list/view.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ListItem } from "../../components/list-item/view";
 import { IconButton } from '../../components/ui-button/view';
 import { Input } from "../../components/ui-input/view";
+import api from "../../api";
 import './style.sass';
 
 function List() {
@@ -9,61 +10,82 @@ function List() {
   const [newItemInput, setNewItemInput] = useState("hide");
   const [newItemValue, setNewItemValue] = useState("");
 
+  useEffect(()=> {
+    api.get("/list")
+      .then((response) => {
+        setItems(response.data)
+    })
+  }, []);
+
   return (
     <div className="list-container">
-        <div className="list-info dark">
-            <h1 className='title'> Lista To Do </h1>
-            <IconButton
-              name="add-circle"
-              color="button-dark"
-              function={() => {
-                setNewItemInput("show");
-            }}/>
-        </div>
-        <div className="list-content">
-          { (items.length > 0 || newItemInput === "show") ? items.map((item) => {
+      <div className="list-info dark">
+          <h1 className='title'> Lista To Do </h1>
+          <IconButton
+            name="add-circle"
+            color="button-dark"
+            function={() => {
+              setNewItemInput("show");
+          }}/>
+      </div>
+      <div className="list-content">
+        { (items.length > 0 || newItemInput === "show") ?
+          items.map((item) => {
             return (
-              <ListItem name={item.name} edit={item.edit} key={item.name} function={(content) => {
-                setItems(items.map((i) => {
-                  if (i.name === content.oldName){
-                    return {...i, "name": content.name};
-                  }
-                  return(i);
-                }));
-              }}/>
-            )
-          }) : <h1 className="empty"> Lista Vazia </h1>}
-          <div className={"new-item " + newItemInput}>
-            <form className="new-item-form" onSubmit={(e) => {
-              e.preventDefault();
-              if(newItemValue.length > 0){
-                setItems([...items, {"name": newItemValue, "edit": false}]);
-              }
-              setNewItemValue("");
-            }}>
-              <Input
-                placeholder="Novo item"
-                value={newItemValue}
-                function={(e) => {
-                  setNewItemValue(e.target.value);
-              }}/>
-              <IconButton
-                name="checkmark"
-                color="button-light"
-                type="submit"
-                size="sm"
-              />
-            </form>
-            <IconButton
-              name="close"
-              color="button-light"
-              size="sm"
-              function={() => {
-                setNewItemInput("hide");
-                setNewItemValue("");
+              <ListItem
+                item={item}
+                key={item.name}
+                submit={(content) => {
+                  setItems(items.map((i) => {
+                    if (i.id === content.id){
+                      api.patch(`/list/${content.id}`, {name: content.name}).then(response => setItems(response.data));
+                    }
+                    return(i);
+                }))}}
+                click={(content) => {
+                  setItems(items.map((i) => {
+                    if (i.id === content.id){
+                      api.patch(`/list/${content.id}`, {done: content.done}).then(response => setItems(response.data));
+                    }
+                    return(i);
+                  }))
+                }}
+                delete={(content) => {
+                  api.delete(`/list/${content.id}`).then((response => setItems(response.data)));
+                }}
+          />
+        )}) : <h1 className="empty"> Lista Vazia </h1>}
+        <div className={"new-item " + newItemInput}>
+          <form className="new-item-form" onSubmit={(e) => {
+            e.preventDefault();
+            if(newItemValue.length > 0){
+              api.post(`/list`, {name: newItemValue, edit: false, done: false}).then(response => setItems(response.data));
+            }
+            setNewItemValue("");
+          }}>
+            <Input
+              placeholder="Novo item"
+              value={newItemValue}
+              function={(e) => {
+                setNewItemValue(e.target.value);
             }}/>
-          </div>
+            <IconButton
+              name="checkmark"
+              color="button-light"
+              type="submit"
+              size="sm"
+            />
+          </form>
+          <IconButton
+            name="close"
+            color="button-light"
+            size="sm"
+            function={() => {
+              setNewItemInput("hide");
+              setNewItemValue("");
+          }}/>
         </div>
+      </div>
     </div>
   );
 }
